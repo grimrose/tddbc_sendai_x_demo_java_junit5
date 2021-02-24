1a12d8828373a97b0d87a657a37a4d95129af338
Takehiro Inoue <takehiro.agile@gmail.com>
2021-02-24 22:39:44 +0900

    Refctoring Test

--- src/test/java/tddbc/GridPointTest.java
+++ src/test/java/tddbc/GridPointTest.java
@@ -10,13 +10,13 @@ public class GridPointTest {
     @Nested
     class 文字列表記が取得できる {
         @Test
-        public void x座標が4でy座標が7である格子点の文字列表記を取得する() throws Exception {
+        void x座標が4でy座標が7である格子点の文字列表記を取得する() throws Exception {
             GridPoint sut = new GridPoint(4, 7);
             assertEquals("(4,7)", sut.notation());
         }
 
         @Test
-        public void x座標が3でy座標が8である格子点の文字列表記を取得する() throws Exception {
+        void x座標が3でy座標が8である格子点の文字列表記を取得する() throws Exception {
             GridPoint sut = new GridPoint(3, 8);
             assertEquals("(3,8)", sut.notation());
         }
@@ -25,13 +25,13 @@ public class GridPointTest {
     @Nested
     class 同じ座標を持つか {
         @Test
-        public void 格子点_4_7_と格子点_4_7_は同じ座標を持つ() throws Exception {
+        void 格子点_4_7_と格子点_4_7_は同じ座標を持つ() throws Exception {
             GridPoint sut = new GridPoint(4, 7);
             assertTrue(sut.hasSameCoordinatesWith(new GridPoint(4, 7)));
         }
 
         @Test
-        public void 格子点_4_7_と格子点_3_8_は同じ座標を持たない() throws Exception {
+        void 格子点_4_7_と格子点_3_8_は同じ座標を持たない() throws Exception {
             GridPoint sut = new GridPoint(4, 7);
             assertFalse(sut.hasSameCoordinatesWith(new GridPoint(3, 8)));
         }
@@ -39,45 +39,41 @@ public class GridPointTest {
 
     @Nested
     class 隣り合っているか {
+
+        GridPoint sut = new GridPoint(4, 7);
+
         @Test
-        public void 格子点_4_7_と左隣の格子点_3_7_は隣り合っている() {
-            GridPoint sut = new GridPoint(4, 7);
+        void 格子点_4_7_と左隣の格子点_3_7_は隣り合っている() {
             assertTrue(sut.isNeighborOn(new GridPoint(3, 7)));
         }
 
         @Test
-        public void 格子点_4_7_と右隣の格子点_5_7_は隣り合っている() {
-            GridPoint sut = new GridPoint(4, 7);
+        void 格子点_4_7_と右隣の格子点_5_7_は隣り合っている() {
             assertTrue(sut.isNeighborOn(new GridPoint(5, 7)));
         }
 
         @Test
-        public void 格子点_4_7_と上隣の格子点_4_8_は隣り合っている() {
-            GridPoint sut = new GridPoint(4, 7);
+        void 格子点_4_7_と上隣の格子点_4_8_は隣り合っている() {
             assertTrue(sut.isNeighborOn(new GridPoint(4, 8)));
         }
 
         @Test
-        public void 格子点_4_7_と下隣の格子点_4_6_は隣り合っている() {
-            GridPoint sut = new GridPoint(4, 7);
+        void 格子点_4_7_と下隣の格子点_4_6_は隣り合っている() {
             assertTrue(sut.isNeighborOn(new GridPoint(4, 6)));
         }
 
         @Test
-        public void 格子点_4_7_と同じ位置の格子点_4_7_は隣り合っていない() {
-            GridPoint sut = new GridPoint(4, 7);
+        void 格子点_4_7_と同じ位置の格子点_4_7_は隣り合っていない() {
             assertFalse(sut.isNeighborOn(new GridPoint(4, 7)));
         }
 
         @Test
-        public void 格子点_4_7_と右上の格子点_5_8_は隣り合っていない() {
-            GridPoint sut = new GridPoint(4, 7);
+        void 格子点_4_7_と右上の格子点_5_8_は隣り合っていない() {
             assertFalse(sut.isNeighborOn(new GridPoint(5, 8)));
         }
 
         @Test
-        public void 格子点_4_7_と左下の格子点_3_6_は隣り合っていない() {
-            GridPoint sut = new GridPoint(4, 7);
+        void 格子点_4_7_と左下の格子点_3_6_は隣り合っていない() {
             assertFalse(sut.isNeighborOn(new GridPoint(3, 6)));
         }
     }
